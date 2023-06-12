e96e49277e2b64bcd19e5ead162de62d2d9aa9ea
David Sutton <davidsutton@ausocean.org>
2023-06-12 20:55:25 +0930

    :lock: Add password hashing
    Password hashing using the golang implementation of argon2. The hash, along with the parameters and the salt are stored in the database.

--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
 module github.com/davidsutts/escapyr
 
 go 1.19
+
+require (
+	github.com/golang-sql/civil v0.0.0-20220223132316-b832511892a9 // indirect
+	github.com/golang-sql/sqlexp v0.1.0 // indirect
+	github.com/microsoft/go-mssqldb v1.1.0 // indirect
+	golang.org/x/crypto v0.9.0 // indirect
+	golang.org/x/sys v0.8.0 // indirect
+)
--- login.go
+++ login.go
@@ -1,9 +1,33 @@
 package main
 
 import (
+	"context"
+	"crypto/rand"
+	"crypto/subtle"
+
+	"database/sql"
+	"encoding/base64"
+	"errors"
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
+	"strings"
+
+	"golang.org/x/crypto/argon2"
+)
+
+type params struct {
+	memory      uint32
+	iterations  uint32
+	parallelism uint8
+	saltLength  uint32
+	keyLength   uint32
+}
+
+var (
+	ErrInvalidHash         = errors.New("the encoded hash is not in the correct format")
+	ErrIncompatibleVersion = errors.New("incompatible version of argon2")
 )
 
 func loginHandler(w http.ResponseWriter, r *http.Request) {
@@ -23,8 +47,129 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
 func loginFormHandler(w http.ResponseWriter, r *http.Request) {
 	log.Println(r.URL.Path)
 
+	// Get context.
+	ctx := r.Context()
+
+	// Get form inputs.
 	username := r.FormValue("username")
 	password := r.FormValue("password")
 
-	log.Println("Username:", username, "Password:", password)
+	if checkLogin(username, password, ctx) {
+		w.WriteHeader(http.StatusOK)
+		log.Printf("Successful login attempt for %s", username)
+	} else {
+		w.WriteHeader(http.StatusUnauthorized)
+		w.Write([]byte("Invalid Username or password"))
+		log.Printf("Failed Login attempt")
+	}
+
+}
+
+func checkLogin(uname, pword string, ctx context.Context) bool {
+
+	// Get password hash from db.
+	var dbPwordHash string
+	err := db.QueryRowContext(
+		ctx,
+		"SELECT pword FROM Users WHERE uname = @uname;",
+		sql.Named("uname", uname),
+	).Scan(&dbPwordHash)
+	if err != nil {
+		log.Println("failed to get value:", err)
+		return false
+	}
+
+	// Check if password is correct.
+	match, err := comparePasswordAndHash(pword, dbPwordHash)
+	if err != nil {
+		log.Println("Failed to compare password: %w", err)
+		return false
+	}
+	return match
+
+}
+
+func generateHash(password string, p params) (encodedHash string, err error) {
+	// Generate salt.
+	salt, err := generateSalt(p.saltLength)
+	if err != nil {
+		return "", fmt.Errorf("could not generate salt: %w", err)
+	}
+
+	// Generate hash.
+	hash := argon2.IDKey([]byte(password), salt, p.iterations, p.memory, p.parallelism, p.keyLength)
+
+	// Encode salt and hash.
+	b64Salt := base64.RawStdEncoding.EncodeToString(salt)
+	b64Hash := base64.RawStdEncoding.EncodeToString(hash)
+
+	encodedHash = fmt.Sprintf("$argon2id$v=%d$m=%d,t=%d,p=%d$%s$%s", argon2.Version, p.memory, p.iterations, p.parallelism, b64Salt, b64Hash)
+
+	return encodedHash, nil
+
+}
+
+func comparePasswordAndHash(password, encodedHash string) (match bool, err error) {
+	// Extract the parameters, salt and derived key from the encoded password hash.
+	p, salt, hash, err := decodeHash(encodedHash)
+	if err != nil {
+		return false, err
+	}
+
+	// Derive the key from the other password using the same parameters.
+	otherHash := argon2.IDKey([]byte(password), salt, p.iterations, p.memory, p.parallelism, p.keyLength)
+
+	// Check that the contents of the hashed passwords are identical.
+	if subtle.ConstantTimeCompare(hash, otherHash) == 1 {
+		return true, nil
+	}
+
+	return false, nil
+}
+
+func generateSalt(n uint32) ([]byte, error) {
+	b := make([]byte, n)
+	_, err := rand.Read(b)
+	if err != nil {
+		return nil, err
+	}
+
+	return b, nil
+
+}
+
+func decodeHash(encodedHash string) (p *params, salt, hash []byte, err error) {
+	vals := strings.Split(encodedHash, "$")
+	if len(vals) != 6 {
+		return nil, nil, nil, ErrInvalidHash
+	}
+
+	var version int
+	_, err = fmt.Sscanf(vals[2], "v=%d", &version)
+	if err != nil {
+		return nil, nil, nil, err
+	}
+	if version != argon2.Version {
+		return nil, nil, nil, ErrIncompatibleVersion
+	}
+
+	p = &params{}
+	_, err = fmt.Sscanf(vals[3], "m=%d,t=%d,p=%d", &p.memory, &p.iterations, &p.parallelism)
+	if err != nil {
+		return nil, nil, nil, err
+	}
+
+	salt, err = base64.RawStdEncoding.Strict().DecodeString(vals[4])
+	if err != nil {
+		return nil, nil, nil, err
+	}
+	p.saltLength = uint32(len(salt))
+
+	hash, err = base64.RawStdEncoding.Strict().DecodeString(vals[5])
+	if err != nil {
+		return nil, nil, nil, err
+	}
+	p.keyLength = uint32(len(hash))
+
+	return p, salt, hash, nil
 }
