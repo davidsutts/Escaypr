b49658de5eaf6165099b3929ffbf9191246b9131
David Sutton <davidsutton@ausocean.org>
2023-06-12 22:11:58 +0930

    :children_crossing: Add frontend login feedback
    Users can now login and either be navigated to /index or receive an error message and try again after a delay.

--- static/script/form.ts
+++ static/script/form.ts
@@ -1,40 +1,49 @@
-document.addEventListener("keydown", function(event: KeyboardEvent) {
+// Global variables.
+var loading = false;
+
+// Check for changes on form to update submission button.
+document.addEventListener("DOMContentLoaded", () => {
+	document.getElementById("pword")?.addEventListener("change", toggleSubmit);
+	document.getElementById("uname")?.addEventListener("change", toggleSubmit);
+})
+
+// Submit form when enter is pressed.
+document.addEventListener("keyup", function(event: KeyboardEvent) {
 	var button = document.querySelector<HTMLElement>("#submit");
-	if (checkForm() == false){
-		button?.setAttribute("disabled", "true");
-	} else {
-		button?.removeAttribute("disabled");
-		if (event.code == "Enter") {
+	if (event.code == "Enter") {
+		if (checkForm()) {
 			postForm();
+			button?.setAttribute("disabled", "true");
 		}
+	} else {
+		return;
 	}
 });
 
+// Check the form to ensure the username and password are not blank
 function checkForm() : boolean {
 	let inputs = document.querySelectorAll("input");
 	for (let i = 0; i < 2; i++) {
-		if (inputs[i].value == "") {
+		if (loading || inputs[i].value == "") {
 			return false;
 		}
 	}
 	return true;
 }
 
-function postForm() {
-	var message = document.querySelector<HTMLElement>('p.err');
-
-	if (!checkForm()) {
-		if (message?.style) {
-			message.style.display = "block";
-			message.innerText = "Invalid username or password";
-		}
-		return;
+// Toggle the submit button depending on the checkForm return.
+function toggleSubmit() {
+	var button = document.querySelector<HTMLElement>("#submit");
+	if (checkForm() == false){
+		button?.setAttribute("disabled", "true");
 	} else {
-		if (message?.style) {
-			message.style.display = "none";
-		}
+		button?.removeAttribute("disabled");
 	}
-	
+}
+
+// Post the inputs to the form.
+function postForm() {	
+	const msg = document.querySelector("p")!;
 	const form = document.querySelector('form')!;
 	const url =  "/login/form";
 
@@ -42,6 +51,24 @@ function postForm() {
 
 	let xhr = new XMLHttpRequest;
 	xhr.open("POST", url);
+
+	// Load response and redirect or fail attempt.
+	xhr.onloadstart = () => {loading = true;}
+	xhr.onloadend = () => {
+		msg.style.display = "block";
+		if (xhr.status == 200) {
+			msg.innerText = "Login Successful";
+			loading = false;
+			window.location.replace("/index")	// Send logged in user to index.
+		} else {
+			msg.innerText = "Invalid usernamne or password";
+			setTimeout(() => {
+				loading = false;
+			}, 1000);	// Make user wait before trying again.
+		}
+	}
+
+	// Send login request.
 	xhr.send(formData);
 
 }
